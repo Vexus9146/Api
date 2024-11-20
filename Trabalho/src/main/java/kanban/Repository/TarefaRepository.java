792e2d4b961d08abb35414f698a6deded2e68803
Vinícius Seifert Fonceca <151111005+Vexus9146@users.noreply.github.com>
2024-11-19 21:43:56 -0300

    Add files via upload

---
+++ Trabalho/src/main/java/kanban/Repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package kanban.Repository;
+import kanban.Model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+}
